6acbdf330984aa8644cd9cf47f23c526a2dcb9fd
Spencer Kendall <spencerlkendall@gmail.com>
2023-03-01 18:03:06 -0600

    final changes

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -7,6 +7,12 @@
 */
 
 //CODE HERE
+let lovesCode = true;
+if (lovesCode = true ){
+  console.log("I love to code!")
+} else {
+  console.log("Coding has its challenges.")
+};
 
 // For problems 2-3 use the following lines of code:
 var amysAge = 29;
@@ -17,31 +23,61 @@ var brittanisBirthYear = 1986;
 ////////// PROBLEM 2 //////////
 
 /*
-  Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
+  Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. 
+  Hint: Consider what will happen if they are the same age. Handle this situation.
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge) {
+  console.log(`Amy is older`)
+} else if (brittanisAge > amysAge) {
+  console.log(`Brittani is older`)
+} else {
+  console.log ("They are the same age.")
+}
 ////////// PROBLEM 3 //////////
 
 /*
-  Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
+  Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. 
+  If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
 */
 
 //CODE HERE
-
+if (amysBirthYear === brittanisBirthYear) {
+  console.log("Amy & Brittani were born in the same year")
+} else {
+  console.log("Amy & Brittani were not born in the same year")
+}
 ////////// PROBLEM 4 //////////
 
 let temperature = 55;
 let rain = true;
 
 /*
-  Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
-  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+  Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, 
+  based on the above temperature and rain.
+  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. 
+  If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. 
+  If it is 60 degrees or below, one should wear a jacket and carry an umbrella. 
+  Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
 */
 
 //CODE HERE
-
+if (temperature >= 80 && rain === true ) {
+  console.log("BEEP BOOP IT IS *RAINING* AND *WARM* YOU SHOULD TAKE A *T-SHIRT* AND *AN UMBRELLA* BEEP BOOP")
+} else if (temperature < 80 && temperature > 60 && rain === true) {
+  console.log("BEEP BOOP IT IS *RAINING* AND *FAIR* YOU SHOULD TAKE A *RAIN JACKET* BEEP BOOP")
+} else if (temperature <= 60 && rain === true) {
+  console.log("BEEP BOOP IT IS *RAINING* AND *CHILLY* YOU SHOULD TAKE A *JACKET* AND *AN UMBRELLA* BEEP BOOP")
+} else if (temperature >= 80 && rain === false) {
+  console.log("BEEP BOOP IT IS *CLEAR* AND *WARM* YOU SHOULD TAKE A *T-SHIRT* AND *SUNGLASSES* BEEP BOOP")
+} else if (temperature < 80 && temperature > 60 && rain === false) {
+  console.log("BEEP BOOP IT IS *CLEAR* AND *FAIR* YOU SHOULD TAKE A *LONG-SLEEVE* AND *SUNGLASSES* BEEP BOOP")
+} else if (temperature <= 60 && rain === false) {
+  console.log("BEEP BOOP IT IS *CLEAR* AND *FAIR* YOU SHOULD TAKE A *JACKET* AND *SUNGLASSES* BEEP BOOP")
+} else {
+  console.log("BEEP BOOP ERROR IN DETECTING WEATHER CHECK WIFI CONNECTION BEEP BOOP")
+}
 ////////// PROBLEM 5 //////////
 
 /*
@@ -49,6 +85,9 @@ let rain = true;
 */
 
 //CODE HERE
+for (let i = 1; i <= 10; i++) {
+  console.log('hello')
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -57,7 +96,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (let i = 1; i <= 10; i++) {
+  console.log(i)
+}
 ////////// PROBLEM 7 //////////
 
 /*
@@ -65,7 +106,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (let i = 10; i >= 0; i -= 1 ) {
+  console.log(i)
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -73,11 +116,15 @@ let score = 0;
 let passingScore = 7;
 
 /*
-  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. 
+  If your code is successful, you will see your logged statement 7 times.
 */
 
 //CODE HERE
-
+while (score < 7) {
+  console.log("Your score is not high enough")
+  score += 1
+}
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
